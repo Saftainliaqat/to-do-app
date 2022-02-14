e77b3774ab43523a348a5079bb3ff28c0af89540
Saftain <saftainliaqat9764@gmail.com>
2022-02-14 05:25:46 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/header'
+import SubContainer from './components/subcontainer'
+
+function App() {
+  return (
+    <div className="App"  style={{background: "#80b3ff"}} >
+      <Header />
+      <SubContainer />
+    </div>
+  );
+}
+
+export default App;
